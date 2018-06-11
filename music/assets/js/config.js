53fef7466d202492d8b2e44bd5bcd5062f217a99
glicOne <romaniuk.cv@gmail.com>
2018-06-11 23:30:32 +0300

    Add pictures

--- music/assets/js/config.js
+++ music/assets/js/config.js
@@ -51,14 +51,14 @@ function aplayer () {
             name: 'Let Me Down Slowly',
             artist: 'Alec Benjamin',
             url: 'https://drive.google.com/uc?export=download&id=1sv9f57RlnoGRJxZMxDjS2SNwLR7fJF3b',
-            cover: '',
+            cover: 'https://drive.google.com/uc?export=download&id=1cejFNJc5vS0Z1S93sTpqJkDdOTUJaa7l',
             lrc: '',
             theme: '#ebd0c2'
         }, {
             name: 'Bad Karma',
             artist: 'Axel Thesleff',
             url: 'https://drive.google.com/uc?export=download&id=1WBxI-Na09K1qTdqanEEM8pKXyErSda88',
-            cover: '',
+            cover: 'https://drive.google.com/uc?export=download&id=1mbxgyhV4ZLqkB9NMSJ9QZo5UiojR9zAh',
             lrc: '',
             theme: '#46718b'
         }, {
@@ -72,160 +72,160 @@ function aplayer () {
             name: 'Tokyo Drift',
             artist: 'Teriyaki Boyz',
             url: 'https://drive.google.com/uc?export=download&id=1Uc5PKDIf6LC1QhnkIZTQNwSABVeJAUJ_',
-            cover: '',
+            cover: 'https://drive.google.com/uc?export=download&id=1gxLatNDG1Yd2ceXjNSlMsoMp0lsncqjc',
             lrc: '',
             theme: '#505d6b'
         }, {
             name: 'Top Songs',
             artist: 'Skillet',
             url: 'https://drive.google.com/uc?export=download&id=1j27KxrLTaIsWhhKQq6nut0REcV8JRDes',
-            cover: '',
+            cover: 'https://drive.google.com/uc?export=download&id=1MraXTWrm08g9eAayrx7Qk2cUxWabmBnn',
             lrc: '',
             theme: '#505d6b'
         }, {
             name: 'New Face',
             artist: 'PSY',
             url: 'https://drive.google.com/uc?export=download&id=1tL5cyymzw-i2rvNyFH0yoHpX4vYcEymp',
-            cover: '',
+            cover: 'https://drive.google.com/uc?export=download&id=16p8prmFDkpq1eXWH0Qb7grIHrcs9nQxB',
             lrc: '',
             theme: '#505d6b'
         }, {
             name: 'I LUV IT',
             artist: 'PSY',
             url: 'https://drive.google.com/uc?export=download&id=1N_TWPqyX_LMS27ZYNa1p0vWlXvjFh9-6',
-            cover: '',
+            cover: 'https://drive.google.com/uc?export=download&id=1rFSc5gMqNOfZSX7J8x0IlgauSkhDUkke',
             lrc: '',
             theme: '#505d6b'
         }, {
             name: 'GENTLEMAN',
             artist: 'PSY',
             url: 'https://drive.google.com/uc?export=download&id=1k3Inj6b2C_J_Q5WknoRL-OhzLxaCaoBt',
-            cover: '',
+            cover: 'https://drive.google.com/uc?export=download&id=1nUu2jJpiRDpimQYVIlzdTYV2rn69_H0Y',
             lrc: '',
             theme: '#505d6b'
         }, {
             name: 'DADDY',
             artist: 'PSY',
             url: 'https://drive.google.com/uc?export=download&id=1Ndk2BJxkNfWAnXm_nollppHZkuu970lr',
-            cover: '',
+            cover: 'https://drive.google.com/uc?export=download&id=1zS7Sj7xjol3MWsOQxc0kHTRJCTyP2aqL',
             lrc: '',
             theme: '#505d6b'
         }, {
             name: 'Zombie',
             artist: 'Nightcore',
             url: 'https://drive.google.com/uc?export=download&id=1os0GxPuR1ZFkEQtigpx-Sk8w8n0caB4L',
-            cover: '',
+            cover: 'https://drive.google.com/uc?export=download&id=1Yuuki2DPCkmnWuiCRcRD1mNDO9Uadqv5',
             lrc: '',
             theme: '#505d6b'
         }, {
             name: 'Zig Zag',
             artist: 'Nightcore',
             url: 'https://drive.google.com/uc?export=download&id=1xroflm5QLnBze7WmOrwF6LNNx5-54uUC',
-            cover: '',
+            cover: 'https://drive.google.com/uc?export=download&id=14wUDPVEiRBN7yho1AUFADnYZrClnaxxs',
             lrc: '',
             theme: '#505d6b'
         }, {
             name: 'Worst Day of My Life ',
             artist: 'Nightcore',
             url: 'https://drive.google.com/uc?export=download&id=1l_sbw0PjH9Z1zOhQG77LMqnE2zzG84ja',
-            cover: '',
+            cover: 'https://drive.google.com/uc?export=download&id=1kJK9y8-8WhTS-lrvWrv3NRiOHzi-3p_K',
             lrc: '',
             theme: '#505d6b'
         }, {
             name: 'Why Do I',
             artist: 'Nightcore',
             url: 'https://drive.google.com/uc?export=download&id=1vqAAKvFDiRfauhYq-hTZJfrxgQ92ITq-',
-            cover: '',
+            cover: 'https://drive.google.com/uc?export=download&id=16uT2P1VC47cYevQDqxl43Pou0kfEFDr6',
             lrc: '',
             theme: '#505d6b'
         }, {
             name: 'Umbrella',
             artist: 'Nightcore',
             url: 'https://drive.google.com/uc?export=download&id=1llHm8dNA8Gd7H_LKpc9IISt-xCI-YDUc',
-            cover: '',
+            cover: 'https://drive.google.com/uc?export=download&id=1Nqi8iAA9_lIOwGraJ23v3GxozTPOkh2x',
             lrc: '',
             theme: '#505d6b'
         }, {
             name: 'Try everything ',
             artist: 'Nightcore',
             url: 'https://drive.google.com/uc?export=download&id=1iFVDAoo5OLWv5zvW37PMRsW7kZaxOG9D',
-            cover: '',
+            cover: 'https://drive.google.com/uc?export=download&id=1WBMb5P2962IfkQomAmBW03Fs45zh95rn',
             lrc: '',
             theme: '#505d6b'
         }, {
             name: 'The Zombie Song',
             artist: 'Nightcore',
             url: 'https://drive.google.com/uc?export=download&id=13Tq7ppa1UciUChJ1cIt26K3RNLrmBB3L',
-            cover: '',
+            cover: 'https://drive.google.com/uc?export=download&id=1b6mesYUiWZjEm5ctLfnYcBc6Xxo995kQ',
             lrc: '',
             theme: '#505d6b'
         }, {
             name: 'Superhero',
             artist: 'Nightcore',
             url: 'https://drive.google.com/uc?export=download&id=1KgOau-rrE7aA_tKF-TV6BaD3_N8AC-Kv',
-            cover: '',
+            cover: 'https://drive.google.com/uc?export=download&id=1KNn4eo_xcgV3Tvmon12LszZVK732gz2t',
             lrc: '',
             theme: '#505d6b'
         }, {
             name: 'I need a Hero',
             artist: 'Nightcore S',
             url: 'https://drive.google.com/uc?export=download&id=1ohVtkKTJh8QqW7lr1m0fQcG1nCt0HhGl',
-            cover: '',
+            cover: 'https://drive.google.com/uc?export=download&id=10V_8ZjtJhp7DhvK-NU89IvpV3DE7btZr',
             lrc: '',
             theme: '#505d6b'
         }, {
             name: 'Rockefeller Street',
             artist: 'Nightcore',
             url: 'https://drive.google.com/uc?export=download&id=1JNRx5m6cixuesagVdrw5yq78gG8yiM6E',
-            cover: '',
+            cover: 'https://drive.google.com/uc?export=download&id=1JTYKTG5VSalEWPYmHGdiAG1TM2gm-jzb',
             lrc: '',
             theme: '#505d6b'
         }, {
             name: 'Prom Queen',
             artist: 'Nightcore',
             url: 'https://drive.google.com/uc?export=download&id=1LFik6Ib0XyfXfIZ5XHkOP7Us-pQybd9L',
-            cover: '',
+            cover: 'https://drive.google.com/uc?export=download&id=17UUGD7le_KE_sgvOmuo9klxNl8EVwGZl',
             lrc: '',
             theme: '#505d6b'
         }, {
             name: 'Pretending',
             artist: 'Nightcore',
             url: 'https://drive.google.com/uc?export=download&id=1p5fmnzsqA4ACLHknsK2FgKTj4pihg5hg',
-            cover: '',
+            cover: 'https://drive.google.com/uc?export=download&id=1PWVzqwAIEpwjvN_bWGaafKq7iVjhjm09',
             lrc: '',
             theme: '#505d6b'
         }, {
             name: 'Memes',
             artist: 'Nightcore',
             url: 'https://drive.google.com/uc?export=download&id=1u1OrB2qkhAeG3w7ToDurv2ooPaFuryzJ',
-            cover: '',
+            cover: 'https://drive.google.com/uc?export=download&id=1VYLPGNlEW5qNiQuSj8IecfX5ACrJNk3H',
             lrc: '',
             theme: '#505d6b'
         }, {
             name: 'Meet Me On The Battlefield',
             artist: 'Nightcore',
             url: 'https://drive.google.com/uc?export=download&id=10GMUZWvkIC8aH7aJ8qou7QBfEqB4ayJk',
-            cover: '',
+            cover: 'https://drive.google.com/uc?export=download&id=121cNHAxlGTnKzoews18Ku4_EfZPVa1bK',
             lrc: '',
             theme: '#505d6b'
         }, {
             name: 'Legend',
             artist: 'Nightcore',
             url: 'https://drive.google.com/uc?export=download&id=1vRo8hwcJaDHr99klVVy8QSeGqemdIj7h',
-            cover: '',
+            cover: 'https://drive.google.com/uc?export=download&id=18aqI-GknGkPNSSsPun8OtB-FpXak8nip',
             lrc: '',
             theme: '#505d6b'
         }, {
             name: 'Infected',
             artist: 'Nightcore',
             url: 'https://drive.google.com/uc?export=download&id=1eL4qIUj-dy-Or1EITGXzUMfZqOq6otY-',
-            cover: '',
+            cover: 'https://drive.google.com/uc?export=download&id=1q4CWymKo5P7C4O5DeIJoptogT0S4slyX',
             lrc: '',
             theme: '#505d6b'
         }, {
             name: 'Infected (Lyrics)',
             artist: 'Nightcore',
-            url: 'https://drive.google.com/uc?export=download&id=18ripzgykBm18q0XD3jfIp6vp4R31imiF',
+            url: 'https://drive.google.com/uc?export=download&id=1Fnl8lWydVT8ytUPerdPRc409Vc9nyzfZ',
             cover: '',
             lrc: '',
             theme: '#505d6b'
@@ -233,42 +233,42 @@ function aplayer () {
             name: 'Ievan Polkka (VSNS Remix)',
             artist: 'Nightcore',
             url: 'https://drive.google.com/uc?export=download&id=18cxNChCJb7BhfTesRfV8eQVY7vV6INNR',
-            cover: '',
+            cover: 'https://drive.google.com/uc?export=download&id=1G6p4RAxUky4vc1tGPAmpAxhuBcftUQOV',
             lrc: '',
             theme: '#505d6b'
         }, {
             name: 'Blow',
             artist: 'Nightcore',
             url: 'https://drive.google.com/uc?export=download&id=1yG_QDDVqWjEgCSMjT4_EjjC602X945SY',
-            cover: '',
+            cover: 'https://drive.google.com/uc?export=download&id=1rR22sYAzq4CrMe36rDE6Sx9gYVNrrA_w',
             lrc: '',
             theme: '#505d6b'
         }, {
             name: 'Shiva ',
             artist: 'Inkyz',
             url: 'https://drive.google.com/uc?export=download&id=1l9NlqDZGXzqxgkIDKfNDBLToRqPwGXMp',
-            cover: '',
+            cover: 'https://drive.google.com/uc?export=download&id=18Erge2yOGh4daGPboSnE-2KgOePtbnfD',
             lrc: '',
             theme: '#505d6b'
         }, {
             name: '60 minute mix of Caravan Palace',
             artist: 'Caravan Palace',
             url: 'https://drive.google.com/uc?export=download&id=1P4M6Q6JsKgeKyGKa-33hPGYeSGqCJ3mc',
-            cover: '',
+            cover: 'https://drive.google.com/uc?export=download&id=1zvtG1Y7r-FnHRjU-FYnMrGfWTwiU5Qm_',
             lrc: '',
             theme: '#505d6b'
         }, {
             name: 'Best of ELECTRO SWING Mix March 2018',
             artist: 'Nightcore',
             url: 'https://drive.google.com/uc?export=download&id=1uca46Cw1E9lmmIXSBBdQKWcgsSiB_HDZ',
-            cover: '',
+            cover: 'https://drive.google.com/uc?export=download&id=1Go515kCyK_X-HCcK6_No_dhddfWlb8xT',
             lrc: '',
             theme: '#505d6b'
         }, {
             name: 'Ignite',
             artist: 'Nightcore',
             url: 'https://drive.google.com/uc?export=download&id=1H5H_liCchBRtveF0tWz8YYm9Lb2HBhno',
-            cover: '',
+            cover: 'https://drive.google.com/uc?export=download&id=1k5xMIYAL9YT53jkoDE3GhzCkOBjlQ-xF',
             lrc: '',
             theme: '#505d6b'
         }]
