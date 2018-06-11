0a67e7bafe3bd6808a75b980041232da880bd180
glicOne <romaniuk.cv@gmail.com>
2018-06-12 01:41:35 +0300

    Add new tracks

--- music/assets/js/config.js
+++ music/assets/js/config.js
@@ -52,224 +52,396 @@ function aplayer () {
             artist: 'Alec Benjamin',
             url: 'https://drive.google.com/uc?export=download&id=1sv9f57RlnoGRJxZMxDjS2SNwLR7fJF3b',
             cover: 'https://drive.google.com/uc?export=download&id=1cejFNJc5vS0Z1S93sTpqJkDdOTUJaa7l',
-            lrc: '',
             theme: '#ebd0c2'
         }, {
             name: 'Bad Karma',
             artist: 'Axel Thesleff',
             url: 'https://drive.google.com/uc?export=download&id=1WBxI-Na09K1qTdqanEEM8pKXyErSda88',
             cover: 'https://drive.google.com/uc?export=download&id=1mbxgyhV4ZLqkB9NMSJ9QZo5UiojR9zAh',
-            lrc: '',
             theme: '#46718b'
         }, {
             name: 'Break it Down',
             artist: 'Kidz In The Hall',
             url: 'https://drive.google.com/uc?export=download&id=1S_DZnnwLxOKLATYr5v5IXIVbUCkyn1b3',
             cover: 'https://drive.google.com/uc?export=download&id=13DjYrJeySVxttvPJ3vdCVO9Ac-ekWRU_',
-            lrc: '',
             theme: '#505d6b'
         }, {
             name: 'Tokyo Drift',
             artist: 'Teriyaki Boyz',
             url: 'https://drive.google.com/uc?export=download&id=1Uc5PKDIf6LC1QhnkIZTQNwSABVeJAUJ_',
             cover: 'https://drive.google.com/uc?export=download&id=1gxLatNDG1Yd2ceXjNSlMsoMp0lsncqjc',
-            lrc: '',
             theme: '#505d6b'
         }, {
             name: 'Top Songs',
             artist: 'Skillet',
             url: 'https://drive.google.com/uc?export=download&id=1j27KxrLTaIsWhhKQq6nut0REcV8JRDes',
             cover: 'https://drive.google.com/uc?export=download&id=1MraXTWrm08g9eAayrx7Qk2cUxWabmBnn',
-            lrc: '',
             theme: '#505d6b'
         }, {
             name: 'New Face',
             artist: 'PSY',
             url: 'https://drive.google.com/uc?export=download&id=1tL5cyymzw-i2rvNyFH0yoHpX4vYcEymp',
             cover: 'https://drive.google.com/uc?export=download&id=16p8prmFDkpq1eXWH0Qb7grIHrcs9nQxB',
-            lrc: '',
             theme: '#505d6b'
         }, {
             name: 'I LUV IT',
             artist: 'PSY',
             url: 'https://drive.google.com/uc?export=download&id=1N_TWPqyX_LMS27ZYNa1p0vWlXvjFh9-6',
             cover: 'https://drive.google.com/uc?export=download&id=1rFSc5gMqNOfZSX7J8x0IlgauSkhDUkke',
-            lrc: '',
             theme: '#505d6b'
         }, {
             name: 'GENTLEMAN',
             artist: 'PSY',
             url: 'https://drive.google.com/uc?export=download&id=1k3Inj6b2C_J_Q5WknoRL-OhzLxaCaoBt',
             cover: 'https://drive.google.com/uc?export=download&id=1nUu2jJpiRDpimQYVIlzdTYV2rn69_H0Y',
-            lrc: '',
             theme: '#505d6b'
         }, {
             name: 'DADDY',
             artist: 'PSY',
             url: 'https://drive.google.com/uc?export=download&id=1Ndk2BJxkNfWAnXm_nollppHZkuu970lr',
             cover: 'https://drive.google.com/uc?export=download&id=1zS7Sj7xjol3MWsOQxc0kHTRJCTyP2aqL',
-            lrc: '',
             theme: '#505d6b'
         }, {
             name: 'Zombie',
             artist: 'Nightcore',
             url: 'https://drive.google.com/uc?export=download&id=1os0GxPuR1ZFkEQtigpx-Sk8w8n0caB4L',
             cover: 'https://drive.google.com/uc?export=download&id=1Yuuki2DPCkmnWuiCRcRD1mNDO9Uadqv5',
-            lrc: '',
             theme: '#505d6b'
         }, {
             name: 'Zig Zag',
             artist: 'Nightcore',
             url: 'https://drive.google.com/uc?export=download&id=1xroflm5QLnBze7WmOrwF6LNNx5-54uUC',
             cover: 'https://drive.google.com/uc?export=download&id=14wUDPVEiRBN7yho1AUFADnYZrClnaxxs',
-            lrc: '',
             theme: '#505d6b'
         }, {
             name: 'Worst Day of My Life ',
             artist: 'Nightcore',
             url: 'https://drive.google.com/uc?export=download&id=1l_sbw0PjH9Z1zOhQG77LMqnE2zzG84ja',
             cover: 'https://drive.google.com/uc?export=download&id=1kJK9y8-8WhTS-lrvWrv3NRiOHzi-3p_K',
-            lrc: '',
             theme: '#505d6b'
         }, {
             name: 'Why Do I',
             artist: 'Nightcore',
             url: 'https://drive.google.com/uc?export=download&id=1vqAAKvFDiRfauhYq-hTZJfrxgQ92ITq-',
             cover: 'https://drive.google.com/uc?export=download&id=16uT2P1VC47cYevQDqxl43Pou0kfEFDr6',
-            lrc: '',
             theme: '#505d6b'
         }, {
             name: 'Umbrella',
             artist: 'Nightcore',
             url: 'https://drive.google.com/uc?export=download&id=1llHm8dNA8Gd7H_LKpc9IISt-xCI-YDUc',
             cover: 'https://drive.google.com/uc?export=download&id=1Nqi8iAA9_lIOwGraJ23v3GxozTPOkh2x',
-            lrc: '',
             theme: '#505d6b'
         }, {
             name: 'Try everything ',
             artist: 'Nightcore',
             url: 'https://drive.google.com/uc?export=download&id=1iFVDAoo5OLWv5zvW37PMRsW7kZaxOG9D',
             cover: 'https://drive.google.com/uc?export=download&id=1WBMb5P2962IfkQomAmBW03Fs45zh95rn',
-            lrc: '',
             theme: '#505d6b'
         }, {
             name: 'The Zombie Song',
             artist: 'Nightcore',
             url: 'https://drive.google.com/uc?export=download&id=13Tq7ppa1UciUChJ1cIt26K3RNLrmBB3L',
             cover: 'https://drive.google.com/uc?export=download&id=1b6mesYUiWZjEm5ctLfnYcBc6Xxo995kQ',
-            lrc: '',
             theme: '#505d6b'
         }, {
             name: 'Superhero',
             artist: 'Nightcore',
             url: 'https://drive.google.com/uc?export=download&id=1KgOau-rrE7aA_tKF-TV6BaD3_N8AC-Kv',
             cover: 'https://drive.google.com/uc?export=download&id=1KNn4eo_xcgV3Tvmon12LszZVK732gz2t',
-            lrc: '',
             theme: '#505d6b'
         }, {
             name: 'I need a Hero',
             artist: 'Nightcore S',
             url: 'https://drive.google.com/uc?export=download&id=1ohVtkKTJh8QqW7lr1m0fQcG1nCt0HhGl',
             cover: 'https://drive.google.com/uc?export=download&id=10V_8ZjtJhp7DhvK-NU89IvpV3DE7btZr',
-            lrc: '',
             theme: '#505d6b'
         }, {
             name: 'Rockefeller Street',
             artist: 'Nightcore',
             url: 'https://drive.google.com/uc?export=download&id=1JNRx5m6cixuesagVdrw5yq78gG8yiM6E',
             cover: 'https://drive.google.com/uc?export=download&id=1JTYKTG5VSalEWPYmHGdiAG1TM2gm-jzb',
-            lrc: '',
             theme: '#505d6b'
         }, {
             name: 'Prom Queen',
             artist: 'Nightcore',
             url: 'https://drive.google.com/uc?export=download&id=1LFik6Ib0XyfXfIZ5XHkOP7Us-pQybd9L',
             cover: 'https://drive.google.com/uc?export=download&id=17UUGD7le_KE_sgvOmuo9klxNl8EVwGZl',
-            lrc: '',
             theme: '#505d6b'
         }, {
             name: 'Pretending',
             artist: 'Nightcore',
             url: 'https://drive.google.com/uc?export=download&id=1p5fmnzsqA4ACLHknsK2FgKTj4pihg5hg',
             cover: 'https://drive.google.com/uc?export=download&id=1PWVzqwAIEpwjvN_bWGaafKq7iVjhjm09',
-            lrc: '',
             theme: '#505d6b'
         }, {
             name: 'Memes',
             artist: 'Nightcore',
             url: 'https://drive.google.com/uc?export=download&id=1u1OrB2qkhAeG3w7ToDurv2ooPaFuryzJ',
             cover: 'https://drive.google.com/uc?export=download&id=1VYLPGNlEW5qNiQuSj8IecfX5ACrJNk3H',
-            lrc: '',
             theme: '#505d6b'
         }, {
             name: 'Meet Me On The Battlefield',
             artist: 'Nightcore',
             url: 'https://drive.google.com/uc?export=download&id=10GMUZWvkIC8aH7aJ8qou7QBfEqB4ayJk',
             cover: 'https://drive.google.com/uc?export=download&id=121cNHAxlGTnKzoews18Ku4_EfZPVa1bK',
-            lrc: '',
             theme: '#505d6b'
         }, {
             name: 'Legend',
             artist: 'Nightcore',
             url: 'https://drive.google.com/uc?export=download&id=1vRo8hwcJaDHr99klVVy8QSeGqemdIj7h',
             cover: 'https://drive.google.com/uc?export=download&id=18aqI-GknGkPNSSsPun8OtB-FpXak8nip',
-            lrc: '',
             theme: '#505d6b'
         }, {
             name: 'Infected',
             artist: 'Nightcore',
             url: 'https://drive.google.com/uc?export=download&id=1eL4qIUj-dy-Or1EITGXzUMfZqOq6otY-',
             cover: 'https://drive.google.com/uc?export=download&id=1q4CWymKo5P7C4O5DeIJoptogT0S4slyX',
-            lrc: '',
             theme: '#505d6b'
         }, {
             name: 'Infected (Lyrics)',
             artist: 'Nightcore',
             url: 'https://drive.google.com/uc?export=download&id=1Fnl8lWydVT8ytUPerdPRc409Vc9nyzfZ',
             cover: '',
-            lrc: '',
             theme: '#505d6b'
         }, {
             name: 'Ievan Polkka (VSNS Remix)',
             artist: 'Nightcore',
             url: 'https://drive.google.com/uc?export=download&id=18cxNChCJb7BhfTesRfV8eQVY7vV6INNR',
             cover: 'https://drive.google.com/uc?export=download&id=1G6p4RAxUky4vc1tGPAmpAxhuBcftUQOV',
-            lrc: '',
             theme: '#505d6b'
         }, {
             name: 'Blow',
             artist: 'Nightcore',
             url: 'https://drive.google.com/uc?export=download&id=1yG_QDDVqWjEgCSMjT4_EjjC602X945SY',
             cover: 'https://drive.google.com/uc?export=download&id=1rR22sYAzq4CrMe36rDE6Sx9gYVNrrA_w',
-            lrc: '',
             theme: '#505d6b'
         }, {
             name: 'Shiva ',
             artist: 'Inkyz',
             url: 'https://drive.google.com/uc?export=download&id=1l9NlqDZGXzqxgkIDKfNDBLToRqPwGXMp',
             cover: 'https://drive.google.com/uc?export=download&id=18Erge2yOGh4daGPboSnE-2KgOePtbnfD',
-            lrc: '',
             theme: '#505d6b'
         }, {
             name: '60 minute mix of Caravan Palace',
             artist: 'Caravan Palace',
             url: 'https://drive.google.com/uc?export=download&id=1P4M6Q6JsKgeKyGKa-33hPGYeSGqCJ3mc',
             cover: 'https://drive.google.com/uc?export=download&id=1zvtG1Y7r-FnHRjU-FYnMrGfWTwiU5Qm_',
-            lrc: '',
             theme: '#505d6b'
         }, {
             name: 'Best of ELECTRO SWING Mix March 2018',
             artist: 'Nightcore',
             url: 'https://drive.google.com/uc?export=download&id=1uca46Cw1E9lmmIXSBBdQKWcgsSiB_HDZ',
             cover: 'https://drive.google.com/uc?export=download&id=1Go515kCyK_X-HCcK6_No_dhddfWlb8xT',
-            lrc: '',
             theme: '#505d6b'
         }, {
             name: 'Ignite',
             artist: 'Nightcore',
             url: 'https://drive.google.com/uc?export=download&id=1H5H_liCchBRtveF0tWz8YYm9Lb2HBhno',
             cover: 'https://drive.google.com/uc?export=download&id=1k5xMIYAL9YT53jkoDE3GhzCkOBjlQ-xF',
-            lrc: '',
+            theme: '#505d6b'
+        }, {
+            name: 'Maiden',
+            artist: 'Alix Perez & Ivy Lab',
+            url: 'https://drive.google.com/uc?export=download&id=16DGD4K8Ow4hsU5oTRdClUZZ9LFlaowS_',
+            cover: '',
+            theme: '#505d6b'
+        }, {
+            name: 'l Desert Trap Mix',
+            artist: 'Arabian Trap Music',
+            url: 'https://drive.google.com/uc?export=download&id=1ITh-G33wAJLiwS5bDHj_s-XJqlu9b7nR',
+            cover: '',
+            theme: '#505d6b'
+        }, {
+            name: 'My Hero Academia Main Theme',
+            artist: 'Boku no Hero Academia OST #03',
+            url: 'https://drive.google.com/uc?export=download&id=1LzJnvmX_nIXpNzfTTFKXW-UI7vIpFPVP',
+            cover: '',
+            theme: '#505d6b'
+        }, {
+            name: 'Need for Speed',
+            artist: 'Camaro Trap Music',
+            url: 'https://drive.google.com/uc?export=download&id=1VuBpb500IhS0aThhiG0ch3bXYpBc76e7',
+            cover: '',
+            theme: '#505d6b'
+        }, {
+            name: 'ROCK COMPILATION',
+            artist: 'DIABOLICAL NIGHTCORE',
+            url: 'https://drive.google.com/uc?export=download&id=1qce0Y8jJ3-7a_MBANFeq6QgJBjgZVZYC',
+            cover: '',
+            theme: '#505d6b'
+        }, {
+            name: 'Flosstradamus',
+            artist: 'MVP Feat. Smokepurpp (NGHTMRE Remix)',
+            url: 'https://drive.google.com/uc?export=download&id=1VhZibSRHiaq_eVcEk8mPnG7HV4hsjprr',
+            cover: '',
+            theme: '#505d6b'
+        }, {
+            name: 'Thirsty',
+            artist: 'Ivy Lab',
+            url: 'https://drive.google.com/uc?export=download&id=1StbzwVmCl4pgByCv27emRRwliPfc6dqZ',
+            cover: '',
+            theme: '#505d6b'
+        }, {
+            name: 'Spitfire',
+            artist: 'Infected Mushroom',
+            url: 'https://drive.google.com/uc?export=download&id=1mXTbFg9BQl_nyKTVdidz7kW1jTfz6jaQ',
+            cover: '',
+            theme: '#505d6b'
+        }, {
+            name: 'Home ',
+            artist: 'Inkyz & Ake',
+            url: 'https://drive.google.com/uc?export=download&id=1g3YhL0qQWCad0MKlEGBjpXSCpUvv9t7-',
+            cover: '',
+            theme: '#505d6b'
+        }, {
+            name: 'Dev',
+            artist: 'Inkyz',
+            url: 'https://drive.google.com/uc?export=download&id=147yVvgHm4-Wpn90gLFVIqS6uZoJIMmnZ',
+            cover: '',
+            theme: '#505d6b'
+        }, {
+            name: 'Mandala',
+            artist: 'Inkyz',
+            url: 'https://drive.google.com/uc?export=download&id=1zTaqIeGFhYO-ESiI7t_aouJdabzQ4Rt0',
+            cover: '',
+            theme: '#505d6b'
+        }, {
+            name: 'Umani',
+            artist: 'Inkyz',
+            url: 'https://drive.google.com/uc?export=download&id=1KnJ-vPWflMNr2SM0PuzR_D3WIO2bD1A8',
+            cover: '',
+            theme: '#505d6b'
+        }, {
+            name: 'ssuss',
+            artist: 'Insightful',
+            url: 'https://drive.google.com/uc?export=download&id=1fGH2i6UZ4bY1ElsKxeXo92DvR2Gf91SB',
+            cover: '',
+            theme: '#505d6b'
+        }, {
+            name: 'Fortuna',
+            artist: 'Ivy Lab & Alix Perez',
+            url: 'https://drive.google.com/uc?export=download&id=16Rl69p-2xiDDOtF8UQwPtajFOFUZe0H8',
+            cover: '',
+            theme: '#505d6b'
+        }, {
+            name: 'Cake',
+            artist: 'Ivy Lab',
+            url: 'https://drive.google.com/uc?export=download&id=1uRM6FDnhZLH0K_m4Ncj96zUSmTjHF7na',
+            cover: '',
+            theme: '#505d6b'
+        }, {
+            name: 'Deus',
+            artist: 'Ivy Lab',
+            url: 'https://drive.google.com/uc?export=download&id=1_h0_JcMtDxMRlGZn_XmisauSP5bHFrz_',
+            cover: '',
+            theme: '#505d6b'
+        }, {
+            name: 'Rorschach',
+            artist: 'Ivy Lab',
+            url: 'https://drive.google.com/uc?export=download&id=1ApEAZBWVOv3nKfhUV7uIq5_ILX_xHSYn',
+            cover: '',
+            theme: '#505d6b'
+        }, {
+            name: 'Socket',
+            artist: 'Ivy Lab',
+            url: 'https://drive.google.com/uc?export=download&id=1MPyHoI0Z77iqPAzjRIKnAnyHn4PDQ3Lv',
+            cover: '',
+            theme: '#505d6b'
+        }, {
+            name: 'Two By Two',
+            artist: 'Ivy Lab',
+            url: 'https://drive.google.com/uc?export=download&id=1ljEGeuS0VE_IVC0fTE_zfsHWJYDNvMbP',
+            cover: '',
+            theme: '#505d6b'
+        }, {
+            name: 'Ubane',
+            artist: 'Ivy Lab',
+            url: 'https://drive.google.com/uc?export=download&id=1IbJsIxxxbeQaxMzLPZp861FefBf3xuH3',
+            cover: '',
+            theme: '#505d6b'
+        }, {
+            name: 'YALLAH',
+            artist: 'Jumpa x Reddy Roc',
+            url: 'https://drive.google.com/uc?export=download&id=14K2ZUITS6YqH0kYXXWPfKdXFYlg5k62Q',
+            cover: '',
+            theme: '#505d6b'
+        }, {
+            name: 'John Wick Medley',
+            artist: 'Le Castle Vania',
+            url: 'https://drive.google.com/uc?export=download&id=1AF9-34_hInWks8NtflkXMz4RNEdI7e_t',
+            cover: '',
+            theme: '#505d6b'
+        }, {
+            name: 'I Got Love',
+            artist: 'MiyaGi & Эндшпиль feat. Рем Дигга',
+            url: 'https://drive.google.com/uc?export=download&id=1zzYEZ1qim9kYIqXPVVlyDOvfZiDeO9SZ',
+            cover: '',
+            theme: '#505d6b'
+        }, {
+            name: 'Rave in the Grave',
+            artist: 'Nightcore',
+            url: 'https://drive.google.com/uc?export=download&id=1X8RbgupuukkbIQzb4BPZsut_aNeaDt33',
+            cover: '',
+            theme: '#505d6b'
+        }, {
+            name: 'Dont Let Me Down',
+            artist: 'Nightcore',
+            url: 'https://drive.google.com/uc?export=download&id=1a3RKUShTNwd8UbKMmrT7YwWmiOVQQgi4',
+            cover: '',
+            theme: '#505d6b'
+        }, {
+            name: 'Dead Silence',
+            artist: 'Nightstep',
+            url: 'https://drive.google.com/uc?export=download&id=1xh9i2JrFEgHKDBZcMbMWm97vO__B0lVC',
+            cover: '',
+            theme: '#505d6b'
+        }, {
+            name: 'Tentacles',
+            artist: 'Noisia',
+            url: 'https://drive.google.com/uc?export=download&id=1m4j2_sh2ea9nrIkl-GyD_2DM41OIkF_h',
+            cover: '',
+            theme: '#505d6b'
+        }, {
+            name: 'Rockstar ft. 21 Savage',
+            artist: 'Post Malone',
+            url: 'https://drive.google.com/uc?export=download&id=1p1SzGbwT-hMH8dqgeocID4xvWktoS71d',
+            cover: '',
+            theme: '#505d6b'
+        }, {
+            name: 'What Are You Waiting For',
+            artist: 'Stonebank',
+            url: 'https://drive.google.com/uc?export=download&id=1l-V2ddW2WTT28-soK7OOMYIHbUnP6P2T',
+            cover: '',
+            theme: '#505d6b'
+        }, {
+            name: 'Licht und Schatten',
+            artist: 'Tokyo Ghoul',
+            url: 'https://drive.google.com/uc?export=download&id=1LDR5h8-DMGThbbdQm2uYJ1eTyaOQku4K',
+            cover: '',
+            theme: '#505d6b'
+        }, {
+            name: 'Do You',
+            artist: 'TroyBoi',
+            url: 'https://drive.google.com/uc?export=download&id=11TYDoKf5xjaTogo86I0k1yBmGMJDJspH',
+            cover: '',
+            theme: '#505d6b'
+        }, {
+            name: 'Dj Turn It Up',
+            artist: 'Yellow Claw',
+            url: 'https://drive.google.com/uc?export=download&id=1Y9RE-0lDt609vFJWlLBk-QSTP0NEw6J2',
+            cover: '',
+            theme: '#505d6b'
+        }, {
+            name: 'Night in Dubai',
+            artist: 'Zwirek',
+            url: 'https://drive.google.com/uc?export=download&id=1hBlLcDJZE2sLTusnoeooCmdfdqRJs8S6',
+            cover: '',
+            theme: '#505d6b'
+        }, {
+            name: 'R3B3L',
+            artist: 'ZwiReK',
+            url: 'https://drive.google.com/uc?export=download&id=1N9PYdrQifKd7Kve-kNe8zDq2-kye3coz',
+            cover: '',
             theme: '#505d6b'
         }]
     });
